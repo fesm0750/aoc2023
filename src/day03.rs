6dc1ac4f4e318e32a8c740957c96878565d6519f
Fernando Martins <emmanuel.martins@outlook.com>
2023-12-03 21:10:22 -0300

    refactor: Refactors day03
    - breaks code into functions;
    - renames variables to improve transmission of intent;
    - creates function to expand borders of the 2-d grid input;
    - remove treatment of special edge cases for borders due to the
    expansion of the grid borders;
    - adds tests;
    - improves comments;
    - changes the `is_adjacent()` method logic to not use subtractions in
    order to avoid subtraction overflow on edge cases;

--- src/day03.rs
+++ src/day03.rs
@@ -8,7 +8,7 @@
 //! - A `symbol` is any character that is neither a digit nor a '.';
 //! - A `number` is a sequence of digits;
 //! - A `part number` is a number adjacent to at least one symbol (above, below, sides, or diagonals);
-//! - A `gear` is represented by a '*' symbol that is adjacent to exactly to part numbers and the `gear ratio` is the
+//! - A `gear` is represented by a '*' symbol that is adjacent exactly to two part numbers and the `gear ratio` is the
 //!   result of multiplying those numbers.
 //!
 //! From an input file containing lines of ascii text representing the aforementioned data:
@@ -19,32 +19,59 @@
 //!
 //! # Solution
 //!
-//! - For part 1: Parse all numbers and stores their start and end positions, check if there is any symbol around the
-//!   expanded rectangle defined by those positions.
+//! - Expanded borders of the input in order to avoid dealing with overflow in boundary cases.
+//!
+//! - For part 1: Parse all numbers and stores their start and end positions, then check for any symbol around the
+//!   extended rectangle defined by those positions.
+//!
 //! - For part 2: Checks the input data for '*' and them compare their positions to the part numbers, if there are
-//!   exactly two adjacent part number, store the gear ratio.
+//!   exactly two adjacent part numbers, store the gear ratio.
 //!
 //! # Commentaries
 //!
-//! - Could have expanded the borders of the input with the neutral character '.', so as to not deal with boundary
-//!   cases.
-//!
-//! - Part 2 solution is O^2 complexity, could improve it
+//! - Part 2 solution has an O^2 complexity, which could be improved.
 use std::fs;
 
 pub fn run() {
     let input = fs::read_to_string("inputs/day03").unwrap();
+    let (extended_grid, _, n_cols) = expand_borders(&input, '.');
+    let part_numbers = find_part_numbers(&extended_grid, n_cols);
+
+    // Part 1
+    println!("Part 01: Sum of part numbers: {}", sum_numbers(&part_numbers));
+
+    // Part 2
+    let gears = find_gears(&extended_grid, &part_numbers);
+    println!("Part 02: Gear ratio sum: {}", sum_gear_ratios(&gears));
+}
+
+/// Takes an `input` string representing a grid of two-dimensional data and expands its borders with the `neutral`
+/// character.
+/// Returns a tuple containing the expanded grid, number of rows and number of columns.
+fn expand_borders(input: &str, neutral: char) -> (String, usize, usize) {
     let n_cols = input.lines().next().unwrap().len(); // number of columns
     let n_rows = input.len() / (n_cols + 1); // number of rows; +1 accounts for '\n'
-    println!("{} {}", n_cols, n_rows);
 
+    // extends borders of the grid
+    let mut grid = String::with_capacity((n_cols + 3) * (n_rows + 2));
+    let border = std::iter::repeat(neutral).take(n_cols + 2).collect::<String>();
+    grid.push_str(&border);
+    grid.push('\n');
+    grid.extend(input.lines().map(|l| format!(".{}.\n", l)));
+    grid.push_str(&border);
+
+    (grid, n_rows + 2, n_cols + 2)
+}
+
+/// Takes an expanded grid string and the number of columns to return a Vec containing the part numbers.
+fn find_part_numbers(expanded_grid: &str, n_cols: usize) -> Vec<Number> {
     // Numbers array and auxiliary variables
     let mut numbers = Vec::<Number>::new();
     let mut number_acc = Vec::<u8>::new();
     let (mut start, mut end) = (0, 0);
 
     // runs over the data storing all possible part numbers
-    for (row, line) in input.lines().enumerate() {
+    for (row, line) in expanded_grid.lines().enumerate() {
         for (col, char) in line.as_bytes().iter().enumerate() {
             // push digits into accumulation buffer
             if char.is_ascii_digit() && col < n_cols {
@@ -66,7 +93,7 @@ pub fn run() {
                 // saves number and its position
                 numbers.push(Number {
                     val: n,
-                    line: row,
+                    row,
                     start,
                     end,
                     is_part: false,
@@ -81,27 +108,23 @@ pub fn run() {
     }
 
     // closure to determine if a character is considered a symbol
-    let is_symbol = |s: &str| -> bool { s.contains(|c: char| c != '.' && !c.is_ascii_digit()) };
+    let contains_symbol = |s: &str| -> bool { s.contains(|c: char| c != '.' && !c.is_ascii_digit()) };
 
     // checks if numbers are part numbers
     for n in &mut numbers {
-        // determine indexes for the lines and characters position to check for symbols
-        // if indexes are past boundaries, use positions of the number itself
-        let start = if n.start > 0 { n.start - 1 } else { n.start };
-        let end = if n.end < n_cols - 1 { n.end + 1 } else { n.end };
+        // Because the grid has been extended, there is no need to deal with boundary conditions.
+        let start = n.start - 1;
+        let end = n.end + 1;
 
-        let line_above = if n.line > 0 { n.line - 1 } else { n.line };
-        let line_below = if n.line < n_rows - 1 { n.line + 1 } else { n.line };
-
-        let idx_curr = n.line * (n_cols + 1);
-        let idx_above = line_above * (n_cols + 1);
-        let idx_below = line_below * (n_cols + 1);
+        let idx_curr = n.row * (n_cols + 1);
+        let idx_above = (n.row - 1) * (n_cols + 1);
+        let idx_below = (n.row + 1) * (n_cols + 1);
 
         // a number is a part number if there is a symbol adjacent to it
-        let is_part = is_symbol(&input[idx_above + start..idx_above + end + 1])     // line above
-            || is_symbol(&input[idx_below + start..idx_below + end + 1])                  // line below
-            || is_symbol(&input[idx_curr + start..idx_curr + start + 1])                  // character to the left
-            || is_symbol(&input[idx_curr + end..idx_curr + end + 1]); // character to the right
+        let is_part = contains_symbol(&expanded_grid[idx_above + start..idx_above + end + 1])     // line above
+        || contains_symbol(&expanded_grid[idx_below + start..idx_below + end + 1])                  // line below
+        || contains_symbol(&expanded_grid[idx_curr + start..idx_curr + start + 1])                  // character to the left
+        || contains_symbol(&expanded_grid[idx_curr + end..idx_curr + end + 1]); // character to the right
 
         if is_part {
             n.is_part = true;
@@ -111,23 +134,20 @@ pub fn run() {
     // keep only part numbers
     numbers.retain(|n| n.is_part);
 
-    let total: u32 = numbers.iter().filter(|n| n.is_part).map(|n| n.val).sum();
+    numbers
+}
 
-    println!("Part 01: Sum of part numbers: {}", total);
-
-    //---
-    // Part 2
-    //---
-
+/// Takes a grid and an array of part numbers to return a Vec of gears.
+fn find_gears(grid: &str, part_numbers: &[Number]) -> Vec<Gear> {
     let mut gears = Vec::<Gear>::new();
 
-    for (row, line) in input.lines().enumerate() {
+    for (row, line) in grid.lines().enumerate() {
         for (col, &char) in line.as_bytes().iter().enumerate() {
             if char == b'*' {
                 // checks adjacency
                 let mut adjacency = Vec::<u32>::new();
                 adjacency.extend(
-                    numbers
+                    part_numbers
                         .iter()
                         .filter(|n| n.is_adjacent(Position::new(row, col)))
                         .map(|n| n.val),
@@ -140,38 +160,49 @@ pub fn run() {
         }
     }
 
-    // obtains the sum of gear ratios
-    let sum: u32 = gears.iter().map(|g| g.ratio).sum();
-    println!("Part 02: Gear ratio sum: {}", sum);
+    gears
 }
 
+/// Returns the sum of all values stored in the `Number` struct
+fn sum_numbers(part_numbers: &[Number]) -> u32 {
+    part_numbers.iter().map(|n| n.val).sum()
+}
+
+/// Returns the sum of all gears ratios
+fn sum_gear_ratios(gears: &[Gear]) -> u32 {
+    gears.iter().map(|g| g.ratio).sum()
+}
+
+/// Struct to store two-dimensional grid positions.
 struct Position {
-    row: u32,
-    col: u32,
+    row: usize,
+    col: usize,
 }
 
+/// Struct representing numbers and part numbers on the grid. It stores the value of the number, the row where it is
+/// located, start and end positions within the row and indicates whether the number is a part number.
 struct Number {
     val: u32,
-    line: usize,
+    row: usize,
     start: usize,
     end: usize,
     is_part: bool,
 }
 
+/// Struct to represent gears and store the gear ratio.
 struct Gear {
     ratio: u32,
 }
 
 impl Position {
+    /// Creates a new `Position` from row and column indexes within grid.
     fn new(row: usize, col: usize) -> Position {
-        Position {
-            row: row as u32,
-            col: col as u32,
-        }
+        Position { row, col }
     }
 }
 
 impl Gear {
+    /// Creates a new `Gear` if it meets the adjacency parameters.
     fn new(adjacency: &[u32]) -> Option<Gear> {
         if adjacency.len() == 2 {
             return Some(Gear {
@@ -184,14 +215,43 @@ impl Gear {
 }
 
 impl Number {
+    /// Checks if a given position is adjacent to a number on the grid.
     fn is_adjacent(&self, symbol: Position) -> bool {
-        let line_above = if self.line > 0 { self.line - 1 } else { self.line } as u32;
-        let start_left = if self.start > 0 { self.start - 1 } else { self.start } as u32;
-        // does not need special case for `end`, because that may not overflow
+        self.row.abs_diff(symbol.row) <= 1  // same row or one above or one below
+            && symbol.col <= (self.end + 1) // must be smaller than end or one after end
+            && (symbol.col >= self.start || self.start.abs_diff(symbol.col) == 1) // must be greater than start or one
+                                                                                  // before start
+    }
+}
 
-        symbol.row >= line_above
-            && symbol.row <= self.line as u32 + 1
-            && symbol.col >= start_left
-            && symbol.col <= self.end as u32 + 1
+//----------
+// Tests
+//----------
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tests() {
+        let input = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+
+        let (grid, _, n_cols) = expand_borders(&input, '.');
+        let part_numbers = find_part_numbers(&grid, n_cols);
+
+        // Part 01
+        assert_eq!(sum_numbers(&part_numbers), 4361);
+
+        // Part 02
+        let gears = find_gears(&grid, &part_numbers);
+        assert_eq!(sum_gear_ratios(&gears), 467835);
     }
 }
